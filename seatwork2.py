aa6b35a43c69122e392ad5dccdbbdfcbc2ccd1b5
* <jzlldomer@gmail.com>
2022-11-12 11:55:27 +0800

    Implemented the menu and it's design

---
+++ seatwork2.py
@@ -0,0 +1,7 @@
+
+#Write a python program for contact tracing:
+
+#Display a menu of options
+print("‿︵‿︵  ୨˚̣̣̣͙୧ - M E N U - ୨˚̣̣̣͙୧   ‿︵‿︵")
+print("\n ↡       1 ➭ Add an item           ↡\n ↡       2 ➭ Search                ↡ \n ↡       3 ➭ Exit (y/n)            ↡\n")
+print ("‿︵‿︵  ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧   ‿︵‿︵")
