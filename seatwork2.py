915df03e4212f59def303cddb3ce021a6f0f6d4c
* <jzlldomer@gmail.com>
2022-11-12 12:52:45 +0800

    Adding user's input

--- seatwork2.py
+++ seatwork2.py
@@ -4,4 +4,18 @@
 #Display a menu of options
 print("‿︵‿︵  ୨˚̣̣̣͙୧ - M E N U - ୨˚̣̣̣͙୧   ‿︵‿︵")
 print("\n ↡       1 ➭ Add an item           ↡\n ↡       2 ➭ Search                ↡ \n ↡       3 ➭ Exit (y/n)            ↡\n")
-print ("‿︵‿︵  ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧   ‿︵‿︵")
+print ("‿︵‿︵  ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧   ‿︵‿︵ \n")
+
+
+
+#Ask the user for input
+user_input =int(input("What do you want to do? Choose from the menu above: \n "))
+    
+    
+print("Please Type in your Personal Data for Contact Tracing")
+name  = input("Full Name: ")
+age = input("Age: ")
+address = input("Address: ")
+number = input("Contact Number: ")
+info = input("Activities(meeting, party, in home, etc.): ")
+print("Saved!")
