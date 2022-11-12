2c8c977db3e4677d193c45547ad98aa717a47b7d
* <jzlldomer@gmail.com>
2022-11-12 15:25:36 +0800

    Finished designs, style and colors

--- seatwork2.py
+++ seatwork2.py
@@ -2,9 +2,7 @@
 #Write a python program for contact tracing:
 
 #Display a menu of options
-print("‿︵‿︵  ୨˚̣̣̣͙୧ - M E N U - ୨˚̣̣̣͙୧   ‿︵‿︵")
-print("\n ↡       1 ➭ Add an item           ↡\n ↡       2 ➭ Search                ↡ \n ↡       3 ➭ Exit (y/n)            ↡\n")
-print ("‿︵‿︵  ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧   ‿︵‿︵ \n")
+menu_options = ("\033[0;36;40m\n‿︵‿︵  ୨˚̣̣̣͙୧ - M E N U - ୨˚̣̣̣͙୧   ‿︵‿︵\n\n ↡\033[1;33;40m       1 ➭ Add an item         \033[0;36;40m  ↡\n ↡  \033[1;32;40m     2 ➭ Search              \033[0;36;40m  ↡ \033[0;36;40m\n ↡     \033[1;31;40m  3 ➭ Exit (y/n)            \033[0;36;40m↡\n\n‿︵‿︵  ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧ ୨˚̣̣̣͙୧   ‿︵‿︵ \n")
 
 #Dictionary
 dict = {
@@ -18,7 +16,8 @@ dict = {
 def get_input():
     while True:
     #Ask the user for input
-        print ("What do you want to do? Choose from the menu above: \n")
+        print(menu_options)
+        print ("\033[1;37;40mWhat do you want to do? Choose from the menu above: \n")
         user_input =input()
         menu = ('1', '2', '3')
         if user_input in menu:
@@ -26,17 +25,21 @@ def get_input():
         else:
             print()
             print("Not in the options! Please Try Again\n")
+            print("\n°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。\n ")
         
 while True:   
     user_input = get_input()
     if user_input == "1":
-        print("Please Type in your Personal Data for Contact Tracing")
+        print("\n°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。\n ")
+        print("\033[1;33;40m    ⇝   C O N T A C T  T R A C I N G   ⇜    \033[1;33;40m\n")
+        print(" \033[1;37;40mPlease Type in your Personal Data for Contact Tracing \n\033[0;33;40m")
         name  = input("Full Name: ")
         age = input("Age: ")
         address = input("Address: ")
         number = input("Contact Number: ")
         info = input("Activities(meeting, party, in home, etc.): ")
-        print("Saved!")
+        print("\033[1;32;40m\n    Saved!")
+        print("\033[1;37;40m\n°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。\n ")
             
             #Stored in Dictionary, dict
         dict ["Full Name"] = name
@@ -47,16 +50,21 @@ while True:
      
      #Option 2       
     elif user_input == "2":
-        print("What key do you want to search? ")    
-        key = input()    
+        key = input("\033[0;33;40m\nWhat key do you want to search?: ")     
         if key in dict:
             print (dict[key])
         else:
-            print ("Not Available")
-        
+            print ("\033[1;31;40m\nNot Available\n")
+            print("\033[1;37;40m\n°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。\n ")
             
+     #Option 3
+     
+    elif user_input == "3":
+        yesno = input("\033[1;31;40mAre you sure? (y/n): ")
+      
+        if yesno == "y":    
+            print ("\033[1;32;40m\nExit - Thank You!")
+            print("\033[1;37;40m\n°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。°。\n ")
+            break
+      
         
-            
-    
-        
-    
