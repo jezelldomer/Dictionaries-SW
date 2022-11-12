a096d63b6b5a279b8c697782e3acb43a3bdeb133
* <jzlldomer@gmail.com>
2022-11-12 13:37:43 +0800

    Implemented the function and while loop

--- seatwork2.py
+++ seatwork2.py
@@ -15,22 +15,39 @@ dict = {
         "Information:" : {}
 }
 
-#Ask the user for input
-user_input =int(input("What do you want to do? Choose from the menu above: \n "))
+def get_input():
+    while True:
+    #Ask the user for input
+        print ("What do you want to do? Choose from the menu above: \n")
+        user_input =input()
+        menu = ('1', '2', '3')
+        if user_input in menu:
+            return user_input
+        else:
+            print()
+            print("Not in the options! Please Try Again\n")
+        
+while True:   
+    user_input = get_input()
+    if user_input == "1":
+        print("Please Type in your Personal Data for Contact Tracing")
+        name  = input("Full Name: ")
+        age = input("Age: ")
+        address = input("Address: ")
+        number = input("Contact Number: ")
+        info = input("Activities(meeting, party, in home, etc.): ")
+        print("Saved!")
+            
+            #Stored in Dictionary, dict
+        dict ["Full Name:"] = name
+        dict ["Age:"] = age
+        dict ["Address:"] = address
+        dict ["Contact Number:"] = number
+        dict ["Information:"] = info
+            
+    elif user_input == "2":
+            
+            
     
-print("Please Type in your Personal Data for Contact Tracing")
-name  = input("Full Name: ")
-age = input("Age: ")
-address = input("Address: ")
-number = input("Contact Number: ")
-info = input("Activities(meeting, party, in home, etc.): ")
-print("Saved!")
-
-#Stored in Dictionary, dict
-dict ["Full Name:"] = name
-dict ["Age:"] = age
-dict ["Address:"] = address
-dict ["Contact Number:"] = number
-dict ["Information:"] = info
-
+        
     
